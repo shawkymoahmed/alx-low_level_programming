424bdc66f536d5a3b2a28aff48320d8a134e3610
for coding <78009027+forcoding20@users.noreply.github.com>
2023-02-21 10:45:48 +0300

    Add files via upload

---
+++ 0x02-functions_nested_loops/tests/9-main.c
@@ -0,0 +1,12 @@
+#include "../shawki.h"
+
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	times_table();
+	return (0);
+}
