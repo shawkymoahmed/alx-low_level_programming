a3a57f1ff771fb283e654cde24629eb1dbad5334
for coding <78009027+forcoding20@users.noreply.github.com>
2023-02-23 13:18:52 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,8 +1,3 @@
-/*
- * File: 100-prime_factor.c
- * Auth: Shawky Mohamed
- */
-
 #include <stdio.h>
 #include <math.h>
 
@@ -13,32 +8,32 @@
  */
 int main(void)
 {
- long int n;
- long int max;
- long int i;
+	long int n;
+	long int max;
+	long int i;
 
- n = 612852475143;
- max = -1;
+	n = 612852475143;
+	max = -1;
 
- while (n % 2 == 0)
- {
- max = 2;
- n /= 2;
- }
+	while (n % 2 == 0)
+	{
+		max = 2;
+		n /= 2;
+	}
 
- for (i = 3; i <= sqrt(n); i = i + 2)
- {
- while (n % i == 0)
- {
- max = i;
- n = n / i;
- }
- }
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
 
- if (n > 2)
- max = n;
+	if (n > 2)
+		max = n;
 
- printf("%ld\n", max);
+	printf("%ld\n", max);
 
- return (0);
+	return (0);
 }
