b5d9bbb58c9728760bc1b6f42aee46dc9f71c04f
for coding <78009027+forcoding20@users.noreply.github.com>
2023-03-02 07:17:05 +0300

    Add files via upload

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+/*
+ * File: 7-leet.c
+ * Auth: Shawky Mohamed
+ */
+
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: string to be encoded
+ *
+ * Return: pointer to the encoded string
+ */
+char *leet(char *str)
+{
+int indxOne = 0, indxTwo;
+char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+while (str[indxOne])
+{
+for (indxTwo = 0; indxTwo <= 7; indxTwo++)
+{
+if (str[indxOne] == leet[indxTwo] ||
+str[indxOne] - 32 == leet[indxTwo])
+str[indxOne] = indxTwo + '0';
+}
+
+indxOne++;
+}
+
+return (str);
+}
